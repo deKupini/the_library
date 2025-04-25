f5508df2580c6f5f5e7195b38921ee26c1e04039
kupinskk <kamil.kupinski@contractors.roche.com>
2025-04-25 14:12:00 +0200

    Add Docker and db configuration

--- the_library/settings.py
+++ the_library/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'the_library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', "postgres"),
+        'USER': os.getenv('DB_USER', "postgres"),
+        'PASSWORD': os.getenv('DB_PASSWORD', "postgres"),
+        'HOST': os.getenv('DB_HOST', "localhost"),
+        'PORT': '5432'
     }
 }
 
